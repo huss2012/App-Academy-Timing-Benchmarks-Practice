ae70d04f226d63f7f252b1ebb9a94dc28eac49db
huss2012 <huss24940@gmail.com>
2024-05-06 23:39:29 +0300

    Phase-2 addNums10() method is done.

--- phase-2.js
+++ phase-2.js
@@ -3,7 +3,17 @@ const [addNums, addManyNums] = require("./phase-1");
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
   // Fill this in
-
+  let sumArray = [];
+  for (let i = 0; i <= 9; i++){
+    let incrementMethod;
+    if (increment === 1) {
+      incrementMethod = increment + i;
+    } else {
+      incrementMethod = increment + (i * 10);
+    }
+    sumArray.push(addNums(incrementMethod));
+  }
+  return sumArray;
 }
 
 // Runs `addManyNums` in 10 increasing increments
@@ -11,5 +21,8 @@ function addManyNums10(increment) {
   // Fill this in
 
 }
+
+
+console.log(addNums10(10));
 
 module.exports = [addNums10, addManyNums10];
